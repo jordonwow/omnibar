6d8759475f3d75bc7874ff3d50acc87ac949ad9a
citna <121998391+citna@users.noreply.github.com>
2023-06-02 16:56:24 -0700

    Amplify Curse Tuning Patch
    CD Increased to 45s with talents

--- OmniBar_Mainline.lua
+++ OmniBar_Mainline.lua
@@ -368,7 +368,7 @@ addon.Cooldowns = {
     [353294] = {duration = 60, class = "WARLOCK"}, -- Shadow Rift
     [48020] = {duration = 30, class = "WARLOCK"}, -- Demonic Circle Teleport
     [333889] = {duration = 120, class = "WARLOCK"}, -- Fel Domination
-    [328774] = {duration = 20, class = "WARLOCK"}, -- Amplify Curse
+    [328774] = {duration = 45, class = "WARLOCK"}, -- Amplify Curse
     [410598] = {duration = 60, class = "WARLOCK"}, -- Soul Rip
     [201996] = {duration = 60, class = "WARLOCK"}, -- Call Observer
     [353753] = {duration = 30, class = "WARLOCK"}, -- Bonds of Fel
